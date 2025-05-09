780622e8370209a8f4da7f9f9931cfb0e31ed5d3
Your Name <you@example.com>
2025-05-09 07:50:31 +0100

    Initial Commit

---
+++ BlockYTShortsNew/BlockYTShortsNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockYTShortsNewApp.swift
+//  BlockYTShortsNew
+//
+//  Created by vhalan on 09/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct BlockYTShortsNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
